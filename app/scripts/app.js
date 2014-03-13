96d64265e2e7acc8d72a97b0a81b8d8ef03adf4a
RolandTa <mail@rolandtanner.ch>
2014-03-13 07:31:41 +0000

    init commit

---
+++ app/scripts/app.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/* global app:true */
+
+var app = angular.module('angNewsApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute',
+  'firebase'
+]);
+
+app.constant('FIREBASE_URL', 'https://ang-news-test.firebaseio.com/');
+
+app.config(function ($routeProvider) {
+  $routeProvider
+  .when('/', {
+    templateUrl: 'views/posts.html',
+    controller: 'PostsCtrl'
+  })
+  .when('/posts/:postId', {
+    templateUrl: 'views/showpost.html',
+    controller: 'PostViewCtrl'
+  })
+  .when('/register', {
+    templateUrl: 'views/register.html',
+    controller: 'AuthCtrl'
+  })
+  .when('/login', {
+    templateUrl: 'views/login.html',
+    controller: 'AuthCtrl'
+  })
+  .when('/users/:username', {
+    templateUrl: 'views/profile.html',
+    controller: 'ProfileCtrl'
+  })
+  .otherwise({
+    redirectTo: '/'
+  });
+});
